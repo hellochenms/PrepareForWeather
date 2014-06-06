8eef35c2ba969e9601cc96e57cc549dc1b6f8cee
M2 <iamim2@163.com>
2014-06-06 17:03:00 +0800

    @切换城市；

---
+++ PrepareForWeather/ChangeCitiesView.h
@@ -0,0 +1,13 @@
+//
+//  ChangeCitiesView.h
+//  PrepareForWeather
+//
+//  Created by Chen Meisong on 14-6-6.
+//  Copyright (c) 2014年 Chen Meisong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangeCitiesView : UIView
+
+@end
---
+++ PrepareForWeather/ChangeCitiesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangeCitiesViewController.h
+//  PrepareForWeather
+//
+//  Created by Chen Meisong on 14-6-6.
+//  Copyright (c) 2014年 Chen Meisong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChangeCitiesViewController : UIViewController
+
+@end
